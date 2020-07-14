f69455fb1c495522764386245000fa95d1148c71
Konstantin Baranovskiy <baranovskiykonstantin@gmail.com>
2020-07-14 21:14:13 +0300

    Ukrainian translation complete.

--- help/build_help.mk
+++ help/build_help.mk
@@ -19,10 +19,12 @@ do_nothing :
 MAKMAKE :
 	cshlpcmp.bat RFterm_01.cshlp
 	cshlpcmp.bat RFterm_16.cshlp
+	cshlpcmp.bat RFterm_93.cshlp
 ifeq (WINSCW,$(findstring WINSCW, $(PLATFORM)))
 	md $(EPOCROOT)epoc32\$(PLATFORM)\c\resource\help & \
 	copy out\RFterm_0xae7f53fa.h01 $(EPOCROOT)epoc32\$(PLATFORM)\c\resource\help & \
-	copy out\RFterm_0xae7f53fa.h16 $(EPOCROOT)epoc32\$(PLATFORM)\c\resource\help
+	copy out\RFterm_0xae7f53fa.h16 $(EPOCROOT)epoc32\$(PLATFORM)\c\resource\help & \
+	copy out\RFterm_0xae7f53fa.h93 $(EPOCROOT)epoc32\$(PLATFORM)\c\resource\help
 endif
 
 BLD : do_nothing
@@ -32,6 +34,8 @@ CLEAN :
 	del out\RFterm_0xae7f53fa.h01.hrh
 	del out\RFterm_0xae7f53fa.h16
 	del out\RFterm_0xae7f53fa.h16.hrh
+	del out\RFterm_0xae7f53fa.h93
+	del out\RFterm_0xae7f53fa.h93.hrh
 
 LIB : do_nothing
 
@@ -46,5 +50,6 @@ SAVESPACE : do_nothing
 RELEASABLES :
 	@echo RFterm_0xae7f53fa.h01
 	@echo RFterm_0xae7f53fa.h16
+	@echo RFterm_0xae7f53fa.h93
 
 FINAL : do_nothing
